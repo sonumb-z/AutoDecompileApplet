adc2697dcbdfe26066539768caa3fa7c516c86bf
mht <522119782@qq.com>
2025-03-31 22:23:03 +0800

    ⚡ 输出目录改为绝对路径，增加桌面一键启动bat

--- AutoDecompileApplet.bat
+++ AutoDecompileApplet.bat
@@ -1,4 +1,5 @@
 @echo off
-REM 自行修改项目中main.py路径、小程序缓存目录、输出目录
-python D:\PycharmProjects\AutoDecompileApplet\main.py --appletDir="E:\1qq\WeChat Files\Applet" --outputDir="D:\PycharmProjects\AutoDecompileApplet\result"
+REM 自行修改项目路径、小程序缓存目录、输出目录
+cd /d D:\PycharmProjects\AutoDecompileApplet\
+python main.py --appletDir="E:\1qq\WeChat Files\Applet" --outputDir="D:\PycharmProjects\AutoDecompileApplet\result"
 pause
